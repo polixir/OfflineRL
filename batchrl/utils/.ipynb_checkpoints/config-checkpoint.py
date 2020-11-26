b4929acb20dfe27c4518be13f29eb0bc05f47969
yi <songyigao@gmail.com>
2020-11-26 17:41:30 +0800

    update .gitignore

--- batchrl/utils/.ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,6 +0,0 @@
-def parse_config(cfg_module):
-    args = [ i for i in dir(cfg_module) if not i.startswith("__")]
-    args = { arg: getattr(cfg_module, arg)  for arg in args}
-
-    return args
-    
