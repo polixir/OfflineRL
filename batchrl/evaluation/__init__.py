fa13d37dbc1183c1384cc54fe3c8039eaabf15d1
yi <songyigao@gmail.com>
2021-01-02 20:51:11 +0800

    add callback class

--- batchrl/evaluation/__init__.py
+++ batchrl/evaluation/__init__.py
@@ -1,11 +1,30 @@
 from abc import ABC, abstractmethod
 
 
-class CallBackFn():
-    def __init__(self,):
-        self.index = 0
+class CallBack():
+    def __init__(self,
+                 actor, 
+                 train_buffer, 
+                 callback_type="env",
+                 eval_buffer=None,
+                 args=None,
+                 **kwargs):
+        self.actor = actor
+        self.train_buffer = train_buffer
+        self.eval_buffer_buffer = eval_buffer
+        self.args = args
+        self.kwargs = kwargs
+        
+        self(callback_type)
     
-    @abstractmethod
-    def __call__(self, actor, buffer, **kwargs):
-        pass
+    def __call__(self, callback_type):
+        if callback_type == "env":
+            pass
+        elif callback_type == "fqe":
+            pass
+        elif callback_type == "agmodel":
+            pass
+        else:
+            raise NotImplementedError
+            
         
