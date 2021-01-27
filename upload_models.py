13ca5d735ba512e0562814a0b649444a5d119fe5
Icarus <wizardicarus@gmail.com>
2021-01-27 16:26:56 +0800

    reduce i/o

--- upload_models.py
+++ upload_models.py
@@ -1,15 +1,20 @@
 import os
 import json
+from tqdm import tqdm
+
+def make_dir(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
 if __name__ == '__main__':
-    copy_command_template = 'sshpass -p ubuntu scp -r -o StrictHostKeyChecking=no {source_file} ubuntu@10.200.0.41:~/offline_models/{target_file}'
-    mkdir_command_template = 'sshpass -p ubuntu ssh -o StrictHostKeyChecking=no ubuntu@10.200.0.41 mkdir ~/offline_models/{target_file}'
+    copy_command_template = 'cp {source_file} {target_file}'
 
     aim_folder = 'batchrl_tmp/.aim'
-    for logname in os.listdir(aim_folder):
-        if not 'final' in logname: continue
+    tmp_folder = '/tmp/offline_models'
+    make_dir(tmp_folder)
+
+    for logname in tqdm(filter(lambda x: 'final' in x, os.listdir(aim_folder))):
         log_folder = os.path.join(aim_folder, logname)
-        os.system(mkdir_command_template.format(target_file='/'.join([logname])))
         for exp_id in os.listdir(log_folder):
             if exp_id == 'index': continue
             exp_folder = os.path.join(log_folder, exp_id)
@@ -25,17 +30,19 @@ if __name__ == '__main__':
                     if not "Reward_Mean_Env" in v.keys(): continue
                     gt[k + '.pt'] = v["Reward_Mean_Env"]
 
-                os.system(mkdir_command_template.format(target_file='/'.join([logname, exp_id])))
+                make_dir(os.path.join(tmp_folder, logname, exp_id))
 
                 # save gt
-                gt_file = '/tmp/gt.json'
-                with open(gt_file, 'w') as f:
+                with open(os.path.join(tmp_folder, logname, exp_id, 'gt.json'), 'w') as f:
                     json.dump(gt, f, indent=2)
-                os.system(copy_command_template.format(source_file=gt_file, target_file='/'.join([logname, exp_id, 'gt.json'])))
 
                 # copy model
-                os.system(mkdir_command_template.format(target_file='/'.join([logname, exp_id, 'models'])))
+                make_dir(os.path.join(tmp_folder, logname, exp_id, 'models'))
                 for model_file in gt.keys():
-                    os.system(copy_command_template.format(source_file=os.path.join(exp_folder, 'models', model_file), target_file='/'.join([logname, exp_id, 'models', model_file])))
+                    os.system(copy_command_template.format(
+                        source_file=os.path.join(exp_folder, 'models', model_file), 
+                        target_file=os.path.join(tmp_folder, logname, exp_id, 'models', model_file)))
             except Exception as e:
                 print(e)
+
+    os.system('sshpass -p ubuntu scp -r -o StrictHostKeyChecking=no /tmp/offline_models/* ubuntu@10.200.0.41:~/offline_models/')
