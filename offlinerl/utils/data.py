fix bug of lose track of gradient (torch.tensor will create a new tensor)

offlinerl/utils/data.py
@@ -11,7 +11,7 @@ def to_array_as(x, y):
     if isinstance(x, torch.Tensor) and isinstance(y, np.ndarray):
         return x.detach().cpu().numpy().astype(y.dtype)
     elif isinstance(x, np.ndarray) and isinstance(y, torch.Tensor):
-        return torch.tensor(x).to(y)
+        return torch.as_tensor(x).to(y)
     else:
         return x
     
@@ -58,7 +58,7 @@ class Batch:
 
     def __getattr__(self, key : str) -> Any:
         """Return self.key."""
-        return self.__dict__[key]
+        return getattr(self.__dict__, key)
 
     def __contains__(self, key : str) -> bool:
         """Return key in self."""

offlinerl/utils/net/common.py
@@ -25,7 +25,7 @@ class BasePolicy(ABC):
         pass
     
     def get_action(self, obs):
-        obs_tensor = torch.tensor(obs, device=next(self.parameters()).device, dtype=torch.float32)
+        obs_tensor = torch.as_tensor(obs, device=next(self.parameters()).device, dtype=torch.float32)
         act = to_array_as(self.policy_infer(obs_tensor), obs)
         
         return act

offlinerl/utils/net/continuous.py
@@ -68,7 +68,9 @@ class Critic(nn.Module):
         """Mapping: (s, a) -> logits -> Q(s, a)."""
         s = s.flatten(1)
         if a is not None:
-            a = torch.tensor(a).to(s)
+            if isinstance(a, np.ndarray):
+                a = torch.as_tensor(a)
+            a = a.to(s)
             a = a.flatten(1)
             s = torch.cat([s, a], dim=1)
         logits, h = self.preprocess(s)